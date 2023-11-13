cd49d3ec285c7c79315b88d9449e071de0053b2a
ajay <ajaykrish.krishnanrb@gmail.com>
2023-11-13 12:37:49 +0530

    update chat route

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ from dataclasses import dataclass
 
 
 BOOK = ''
+chats = []
+chatbox_init = [
+    dict(origin="ai",text="Hello, welcome to book shop"), 
+    dict(origin='ai',text="Which book are you looking for today?")
+]
+
 
 
 def read_df(url):
@@ -35,12 +41,6 @@ popular_books = read_df('./popular_books.csv')
 filtered_books = read_df('./filtered_books.csv')
 
 
-chatbox_init = [
-    dict(origin="ai",text="Hello, welcome to book shop"), 
-    dict(origin='ai',text="Which book are you looking for today?")
-]
-
-
 def recommend_books(book_idx, top_k=10):
     score = similarity_matrix[book_idx]
     score = list(enumerate(score))
@@ -61,12 +61,15 @@ def home():
 
 @app.route(rule='/chat', methods=['GET'])
 def chat():
-    if request.args.get('chatbox'):
-        chatbox = json.loads(request.args.get('chatbox'))
+    global chats
+    if request.cookies.get('chatbox', None):
+        chatbox:list = json.loads(request.cookies['chatbox'])
+        chatbox.extend(chats)
+        chats = []
     else:
         chatbox = chatbox_init
     response = make_response(render_template('chat.html', chats=chatbox, cookies=request.cookies))
-    response.set_cookie("chat_box", json.dumps(chatbox), samesite=None)
+    response.set_cookie("chatbox", json.dumps(chatbox), samesite=None)
     return response
 
 
@@ -75,44 +78,42 @@ def chat():
 @app.route(rule='/chat', methods=['POST'])
 def recommend():
     prompt = request.form.get(key='prompt')
-    print(prompt)
-    chatbox=json.loads(request.cookies['chat_box'])
+    chatbox=json.loads(request.cookies['chatbox'])
     if prompt.strip().lower() in ['yes', 'y', 'yeah', 'yup', 'ya', 'n', 'no', 'nope', 'na', 'nei'] :
         if prompt.strip().lower() in ['yes', 'y', 'yeah', 'yup', 'ya']:
             book_name = chatbox[-2]['text'][1:]
             mask = filtered_books['book_title'] ==  book_name
             avg_rating = filtered_books[mask]['Book-Rating'].mean()
             details = filtered_books[mask].iloc[0]
-            chatbox.append(dict(origin='ai', text="Here are some additional information about the book"))
-            chatbox.append(dict(origin='ai',text=f"Book-Author : {str(details['Book-Author'])}"))
-            chatbox.append(dict(origin='ai', text=f"Ratings : {avg_rating :0.2f}"))
-            chatbox.append(dict(origin='ai', text=f"ISBN :, {details['ISBN']}"))
+            chats.append(dict(origin='ai', text="Here are some additional information about the book"))
+            chats.append(dict(origin='ai',text=f"Book-Author : {str(details['Book-Author'])}"))
+            chats.append(dict(origin='ai', text=f"Ratings : {avg_rating :0.2f}"))
+            chats.append(dict(origin='ai', text=f"ISBN :, {details['ISBN']}"))
 
 
-            chatbox.append(dict(origin='ai', text='We have some recommendations for you'))
+            chats.append(dict(origin='ai', text='We have some recommendations for you'))
             recommendation = recommend_books(book_idx=title_map[book_name])
 
             for i in recommendation:
-                chatbox.append(dict(origin='ai', text=f"👉{str(title_inv_map[str(i[0])])}"))
+                chats.append(dict(origin='ai', text=f"👉{str(title_inv_map[str(i[0])])}"))
 
         else:
-            chatbox.append(dict(origin='ai', text="Sorry we couldn't find a match for you."))
-            chatbox.append(dict(origin='ai', text='Try typing the book name alone and be more specific'))
-            chatbox.append(dict(origin='ai', text="Check our popular books for ideas"))
+            chats.append(dict(origin='ai', text="Sorry we couldn't find a match for you."))
+            chats.append(dict(origin='ai', text='Try typing the book name alone and be more specific'))
+            chats.append(dict(origin='ai', text="Check our popular books for ideas"))
         
-        chatbox.append(dict(origin='ai', text="Enter the book name :"))
+        chats.append(dict(origin='ai', text="Enter the book name :"))
     
-        return redirect(f'/chat?chatbox={json.dumps(chatbox)}')
+        return redirect(f'/chat')
 
-    chatbox.append(dict(origin='user', text=prompt))
+    chats.append(dict(origin='user', text=prompt))
     book_title = find_book_title(prompt)
     BOOK = str(book_title[0])
-    chatbox.append(dict(origin='ai', text=f"This is the closest match we found in our database"))
-    chatbox.append(dict(origin="ai", text=f"👉{BOOK}"))
-    chatbox.append(dict(origin="ai", text="Is this the book you are looking for ? Type Y or N"))
+    chats.append(dict(origin='ai', text=f"This is the closest match we found in our database"))
+    chats.append(dict(origin="ai", text=f"👉{BOOK}"))
+    chats.append(dict(origin="ai", text="Is this the book you are looking for ? Type Y or N"))
 
-
-    return redirect(f'/chat?chatbox={json.dumps(chatbox)}')
+    return redirect(f'/chat')
 
 
 
